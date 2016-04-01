aa17014b2eb2372211d3e64eb1fb5598edf3bb04
xuke918 <xuke3070@gmail.com>
2016-04-01 18:02:12 -0400

    Added more keyframes.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
-var $body = $('body');
-var $box = $('.box');
-var $diamond = $('diamond');
-var $panel = $('panel');
-var $circle = $('circle');
-var $list = $('list');
-
-
-$('btn-show-hide').on('click', function() {
-  $(.box).toggleClass('.show');
+$('.btn-show-hide').on('click', function () {
+  $('.box').toggleClass('show');
 });
 
-$('btn-move').on('click', function() {
-  $(.diamond).toggleClass('.show');
+$('.btn-move').on('click', function () {
+  $('.diamond').toggleClass('move');
 });
 
-$('btn-collapse-expand').on('click', function() {
-  $(.panel).toggleClass('.');
+$('.btn-collapse-expand').on('click', function () {
+  $('.panel').toggleClass('collapse');
+});
+
+$('.btn-bounce').on('click', function () {
+  $('.circle').addClass('bounce');
+});
+
+$('.circle').on('animationend', function () {
+  $(this).removeClass('bounce');
+});
+
+$('.btn-append').on('click', function () {
+  $('.list').append('<li> New List Item</li>');
 });
